fa0ac4ca1ff563c4c9a4a2759ac23a0595cef047
andrewmor00 <andrejnovohackij850@gmail.com>
2025-05-24 15:05:13 +0300

    settings

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -6,17 +6,23 @@ function Settings(){
         <div className="settingsPage">
             <Navbar />
             <div>
-                <div className="settingsTxt">
-                    <h3>Личные данные</h3>
-                    <p>Иванов Иван Иванович</p>
-                    <p>email@mail.ru</p>
-                    <p>Отдел эксплуатации систем учета электроэнергии<br/>Ведущий инженер отдела эксплуатации систем учета электроэнергии</p>
-                    <button>Сменить аккаунт</button>
-                </div>
-                <div className="settingsTxt">
-                    <h3>Привязать БД</h3>
-                    <p>Вы можете привязать новое бд или же бд своего района и обновить пользовательские данные</p>
-                    <button>Привязать БД</button>
+                <div>
+                    <div className="settingsTxt">
+                        <h3>Личные данные</h3>
+                        <p>Иванов Иван Иванович</p>
+                        <p>email@mail.ru</p>
+                        <p>Отдел эксплуатации систем учета электроэнергии<br/>Ведущий инженер отдела эксплуатации систем учета электроэнергии</p>
+                        <button>Сменить аккаунт</button>
+                    </div>
+                    <div className="settingsTxt">
+                        <h3>Привязать БД</h3>
+                        <p>Вы можете привязать новое бд или же бд своего района и обновить пользовательские данные</p>
+                        <button>Привязать БД</button>
+                    </div>
+
+                    <div>
+                        <a href="cupertinovets\src\img\mpdf.pdf" download="mpdf.pdf">Скачать инструкцию пользователя</a>
+                    </div>
                 </div>
             </div>
         </div>
